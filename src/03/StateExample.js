8f7af11887fc4757b6124b63f42b841ec6bf9df7
delaying <pgy5638@naver.com>
2023-01-26 11:35:29 +0900

    함수형 컴포넌트와 hooks 적용

---
+++ src/03/StateExample.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function StateExample(){
+    const [loading, setLoading]= useState(true);
+    const [formData, setFormdata]= useState('no data');
+    const data = 'new data';
+
+    useEffect(()=>{
+        let timer = setTimeout(()=>{ setLoading(false); 
+        setFormdata(data+formData); }, 4000);
+
+        return ()=>{ clearTimeout(timer) }
+
+    },[loading]);
+
+    return(
+        <div>
+            <span>로딩중: {String(loading)}</span>
+            <span>결과: {formData}</span>
+        </div>
+    )
+}
+
+export default StateExample;
